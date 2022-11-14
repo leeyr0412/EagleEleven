938a5c17fc97a2a0472ea0637163976f81926674
이예리 <leeyr@iyeliui-MacBookAir.local>
2022-11-14 19:02:22 +0900

    First Commit

---
+++ app/src/main/java/univ/yonsei/eagle_eleven/LoadingActivity.java
@@ -0,0 +1,28 @@
+package univ.yonsei.eagle_eleven;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/univ/yonsei/eagle_eleven/MainActivity.java
@@ -0,0 +1,15 @@
+package univ.yonsei.eagle_eleven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
