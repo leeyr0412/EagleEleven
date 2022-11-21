847a98acd2bdc5ef5f311b75d8d80da58e4e91eb
이예리 <leeyr@iyeliui-MacBookAir.local>
2022-11-21 09:02:58 +0900

    test

--- app/src/main/java/univ/yonsei/eagle_eleven/MainActivity.java
+++ app/src/main/java/univ/yonsei/eagle_eleven/MainActivity.java
@@ -14,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        팀생성, 팀 불러오기
+//        팀 생성
         Button btnTeamMake = findViewById(R.id.btnTeamMake);
+        btnTeamMake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         Button btnTeamLoad = findViewById(R.id.btnTeamLoad);
 
         btnTeamMake.setOnClickListener(new View.OnClickListener() {
