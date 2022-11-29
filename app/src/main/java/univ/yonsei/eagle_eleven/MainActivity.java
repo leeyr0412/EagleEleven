6b2c5ed143317ed46c2d7fc92e691cc02afa0451
yongyong98 <yjjj98@yonsei.ac.kr>
2022-11-29 12:38:40 +0900

    순위 수정20221129

--- app/src/main/java/univ/yonsei/eagle_eleven/MainActivity.java
+++ app/src/main/java/univ/yonsei/eagle_eleven/MainActivity.java
@@ -1,12 +1,15 @@
 package univ.yonsei.eagle_eleven;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,27 +37,17 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout baseLayout; //메인 xml의 부모 레이아웃 id
-    // info class 만들기
-    public class info{
-        public String captainName;
-        public String EmblemUrl;
-        public int gameNum;
-        public int teamNumber;
-        public info(){
-            //default
-        }
-        public info(String captainName,String EmblemUrl,int gameNum,int teamNumber){
-            this.captainName = captainName;
-            this.EmblemUrl = EmblemUrl;
-            this.gameNum = gameNum;
-            this.teamNumber = teamNumber;
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -105,19 +98,45 @@ public class MainActivity extends AppCompatActivity {
         TextView txtFourth = findViewById(R.id.txtFourth);
         TextView txtFifth = findViewById(R.id.txtFifth);
         ArrayList<String> tName = new ArrayList<>();
+
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("TEAM");
-        Query myTopTeam = mDatabase.orderByChild("GameNum");
-        myTopTeam.addValueEventListener(new ValueEventListener() {
+        Query myTopTeam = mDatabase.orderByChild("GameNum").limitToFirst(5);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    android.util.Log.i("snapshot: ",dataSnapshot.getValue().toString());
-                    //tName.add(dataSnapshot.getValue().toString());
-                    tName.add(dataSnapshot.getValue().toString());
-                    //android.util.Log.i("tName: ",tName.toString());
+                    //android.util.Log.i("snapshot: ",dataSnapshot.getValue().toString());
+                    String[] array = dataSnapshot.getValue().toString().split(Pattern.quote("}, "));
+                    HashMap<String,Integer> map = new HashMap<>();
+                    for(int i=0;i<array.length;i++){
+                        //+8 , +9 인 이유는 string으로 받아와서 substring하려고 (게임횟수가 1자리수인경우)
+                        map.put(array[i].substring(0,array[i].indexOf("=")), Integer.valueOf(array[i].substring(array[i].lastIndexOf("GameNum=")+8,array[i].lastIndexOf("GameNum=")+9)));
+                    }
+                    List<Map.Entry<String, Integer>> list_entries = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+                    Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer>>() {
+                        // compare로 값을 비교
+                        public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2)
+                        {
+                            // 내림 차순으로 정렬
+                            return obj2.getValue().compareTo(obj1.getValue());
+                        }
+                    });
+                    for(Map.Entry<String, Integer> entry : list_entries) {
+                        System.out.println(entry.getKey() + " : " + entry.getValue());
+                    }
+                    txtFirst.setText(list_entries.get(0).getKey());
+                    txtSecond.setText(list_entries.get(1).getKey());
+                    txtThird.setText(list_entries.get(2).getKey());
+                    txtFourth.setText(list_entries.get(3).getKey());
+                    txtFifth.setText(list_entries.get(4).getKey());
+                    //tName.add(dataSnapshot.getValue().toString().substring(1,dataSnapshot.getValue().toString().indexOf("=")));
                 }
-                txtFirst.setText(tName.get(0));
-                txtSecond.setText(tName.get(1));
+
+                //tName.add(dataSnapshot.getValue().toString());
+                //txtFirst.setText(tName.get(0));
+                //txtSecond.setText(tName.get(1));
             }
 
             @Override
