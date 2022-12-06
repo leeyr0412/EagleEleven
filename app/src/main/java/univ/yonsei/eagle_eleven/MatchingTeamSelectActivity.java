59fbad4bc8b1dd88eebbfeb137c0a7008ec1277d
yongyong98 <yjjj98@yonsei.ac.kr>
2022-12-06 18:30:10 +0900

    20221206리스트뷰 팀선택

---
+++ app/src/main/java/univ/yonsei/eagle_eleven/MatchingTeamSelectActivity.java
@@ -0,0 +1,14 @@
+package univ.yonsei.eagle_eleven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchingTeamSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching_team_select);
+    }
+}
