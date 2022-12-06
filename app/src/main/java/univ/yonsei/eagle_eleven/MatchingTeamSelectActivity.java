4fcf67985ff3b093ffae9836255bec97b05dc342
yongyong98 <yjjj98@yonsei.ac.kr>
2022-12-06 19:06:05 +0900

    20221206리스트레이아웃

--- app/src/main/java/univ/yonsei/eagle_eleven/MatchingTeamSelectActivity.java
+++ app/src/main/java/univ/yonsei/eagle_eleven/MatchingTeamSelectActivity.java
@@ -2,6 +2,7 @@ package univ.yonsei.eagle_eleven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MatchingTeamSelectActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MatchingTeamSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching_team_select);
+        Intent ListName = getIntent();
+
+        String a = ListName.getStringExtra("ListName");
+        android.util.Log.i("listName: ", a);
     }
 }
