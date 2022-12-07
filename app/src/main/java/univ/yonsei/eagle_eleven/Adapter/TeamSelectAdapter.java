9a5005e77ac159140457efe4569f299857f11776
LeeYeri <leeyr0412@gmail.com>
2022-12-07 15:31:52 +0900

    리스트에서 경기 선택시 매칭 되는거 구현완료

---
+++ app/src/main/java/univ/yonsei/eagle_eleven/Adapter/TeamSelectAdapter.java
@@ -0,0 +1,218 @@
+package univ.yonsei.eagle_eleven.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import univ.yonsei.eagle_eleven.MainActivity;
+import univ.yonsei.eagle_eleven.MakingGameActivity;
+import univ.yonsei.eagle_eleven.MatchingTeamSelectActivity;
+import univ.yonsei.eagle_eleven.Model.Team;
+import univ.yonsei.eagle_eleven.Model.TeamAdapter;
+import univ.yonsei.eagle_eleven.R;
+
+public class TeamSelectAdapter extends RecyclerView.Adapter<TeamSelectAdapter.ViewHolder> {
+    private Context mContext;
+    private List<Team> mTeams;
+//    private FirebaseUser firebaseUser;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    private FirebaseAuth mAuth;     //회원정보 관련
+    FirebaseUser currentUser;
+    String uid;
+
+    @NonNull
+    @Override
+    public TeamSelectAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.team_item,parent,false);
+        return new TeamSelectAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeamSelectAdapter.ViewHolder holder, int position) {
+
+//        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mAuth = FirebaseAuth.getInstance();     //로그인한 유저 회원정보 관련 인스턴스 초기화
+        currentUser = mAuth.getCurrentUser();
+        uid = currentUser.getUid();
+
+
+//        final String yourID;
+
+
+        final Team team = mTeams.get(position);
+        holder.btnTeamSelect.setVisibility(View.VISIBLE);
+        String TeamName = team.getTeamName();   //내 팀 이름
+        holder.teamname.setText(TeamName);
+        holder.txtTeamCaptain.setText("주장 : "+team.getCaptainName());
+        holder.txtGameNum.setText("게임수 : "+team.getGameNum());
+        String TeamNum = ""+team.getTeamNum();
+        holder.txtTeamNumber.setText("팀원수 : "+TeamNum);
+        holder.btnTeamSelect.setText("선택");
+
+        Glide.with(mContext).load(team.getEmblemUrl()).into(holder.image_emblem);
+
+        holder.btnTeamSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                데이터 받아온거 인덱싱 해야함
+                String GameData = ((MatchingTeamSelectActivity)MatchingTeamSelectActivity.mContext).gamedata;
+                Log.i("adapterLog", "GameData: "+GameData);
+//                파싱
+                String date = GameData.substring(0,GameData.indexOf("-"));
+                String yourTeam = GameData.substring(GameData.indexOf("-")+1);
+                Log.i("인덱싱 확인", "date: "+date);
+                Log.i("인덱싱 확인", "yourTeam: "+yourTeam);
+
+//                매칭 됨
+
+//                디비 작업 해야함, 팀 불러와야함
+                View matchingPage = View.inflate(mContext,R.layout.matchingpage,null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(mContext);
+                TextView txtDate = matchingPage.findViewById(R.id.txtDate);
+                TextView txtYourTeam = matchingPage.findViewById(R.id.txtYourTeam);
+                TextView txtMyTeam = matchingPage.findViewById(R.id.txtMyTeam);
+
+//                매칭확인창
+                dlg.setTitle("매치 상세 내용");
+                txtDate.setText(date); // 리스트 내용 dlg 텍스트 뷰에 넣음
+                txtYourTeam.setText(yourTeam);
+                txtMyTeam.setText(TeamName/*내 팀 이름*/);
+                dlg.setView(matchingPage);
+                dlg.setNegativeButton("닫기",null);
+                dlg.show();
+
+
+
+                Button btnReturn = matchingPage.findViewById(R.id.btnReturn);  //매칭하기 버튼
+
+                btnReturn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        히스토리용 테이블에 값 넣기@@@@@@@@@@@@@@@@@@@@!#$###############################
+                        HashMap<String, Object> MyHistory = new HashMap<>();
+                        MyHistory.put("myTeam", ""+TeamName);
+                        MyHistory.put("otherTeam", ""+yourTeam);
+                        MyHistory.put("Date", date);
+                        databaseReference.child("History").child(uid).child(GameData).setValue(MyHistory);
+
+                        HashMap<String, Object> YourHistory = new HashMap<>();
+                        YourHistory.put("myTeam", ""+yourTeam);
+                        YourHistory.put("otherTeam", ""+TeamName);
+                        YourHistory.put("Date", date);
+                        databaseReference.child("GameList").child(GameData).child("UID").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                final String yourID = "" +snapshot.getValue(String.class);
+//                        yourID = ""+s;
+                                databaseReference.child("History").child(yourID).child(GameData).setValue(YourHistory);
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
+
+//                      게임 리스트 디비 삭제하기
+                        DatabaseReference removeReference = FirebaseDatabase.getInstance().getReference("GameList").child(GameData);
+                        removeReference.removeValue();
+
+                        ((Activity)mContext).finish();
+//                        Context context = v.getContext();
+//                        Intent intent = new Intent(context, MakingGameActivity.class/*경기 생성 페이지로 이*/);
+//                        intent.putExtra("TeamName",TeamName);
+//                        intent.putExtra("TeamNum",TeamNum);
+////                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+////                Activity
+////                startA
+//                        context.startActivity(intent);
+                    }
+                });
+
+
+                //items.remove(position);// 매칭 성사 시 리스트뷰에서 삭제
+//                adapter.notifyDataSetChanged();
+//                Toast.makeText(getApplicationContext(),"매칭이 성사 됐습니다.",Toast.LENGTH_SHORT).show();
+
+
+//                팀 불러와서 출력했음. 리스트에서 매칭 삭제
+
+//                히스토리 위한 디비에 각각 대입
+
+
+
+
+//                Context 입ontext = v.getContext();
+//                Intent intent = new Intent(context, MakingGameActivity.class/*경기 생성 페이지로 이*/);
+//                intent.putExtra("TeamName",TeamName);
+//                intent.putExtra("TeamNum",TeamNum);
+//                context.startActivity(intent);
+
+
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return mTeams.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public CircleImageView image_emblem;
+        public TextView teamname;
+        public TextView txtTeamCaptain;
+        public TextView txtTeamNumber;
+        public TextView txtGameNum;
+        public Button btnTeamSelect;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            image_emblem = itemView.findViewById(R.id.image_emblem);
+            teamname = itemView.findViewById(R.id.teamname);
+            txtTeamCaptain = itemView.findViewById(R.id.txtTeamCaptain);
+            txtTeamNumber = itemView.findViewById(R.id.txtTeamNumber);
+            txtGameNum = itemView.findViewById(R.id.txtGameNum);
+            btnTeamSelect = itemView.findViewById(R.id.btnTeamSelect);
+
+        }
+
+    }
+
+    public TeamSelectAdapter(Context mContext, List<Team> mTeams){
+        this.mContext = mContext;
+        this.mTeams = mTeams;
+    }
+}
--- app/src/main/java/univ/yonsei/eagle_eleven/MatchingTeamSelectActivity.java
+++ app/src/main/java/univ/yonsei/eagle_eleven/MatchingTeamSelectActivity.java
@@ -1,11 +1,47 @@
 package univ.yonsei.eagle_eleven;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import univ.yonsei.eagle_eleven.Adapter.TeamSelectAdapter;
+import univ.yonsei.eagle_eleven.Model.Team;
+import univ.yonsei.eagle_eleven.Model.TeamAdapter;
 
 public class MatchingTeamSelectActivity extends AppCompatActivity {
+
+    public static Context mContext;         //adapter에 변수 전달용
+    public String gamedata;
+
+    private FirebaseAuth mAuth;     //회원정보 관련
+    FirebaseUser currentUser;
+    String uid;
+
+    List<String> TeamnameList;
+
+    RecyclerView recyclerView;
+    TeamSelectAdapter teamAdapter;
+    List<Team> teamList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +49,96 @@ public class MatchingTeamSelectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_matching_team_select);
         Intent ListName = getIntent();
 
-        String a = ListName.getStringExtra("ListName");
-        android.util.Log.i("listName: ", a);
+//        변수 받아옴
+        gamedata = ListName.getStringExtra("ListName");
+        Log.i(gamedata, "gamedata: "+gamedata);
+        mContext = this;
+
+        mAuth = FirebaseAuth.getInstance();     //회원정보 관련 인스턴스 초기화
+        currentUser = mAuth.getCurrentUser();
+        uid = currentUser.getUid();
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("경기할 팀 선택");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        recyclerView = findViewById(R.id.recycler_view1);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        teamList = new ArrayList<>();
+        teamAdapter = new TeamSelectAdapter(this, teamList);
+        recyclerView.setAdapter(teamAdapter);
+
+        TeamnameList = new ArrayList<>();
+
+        getTeamlist();
+
+//                공간대여시스템 연결해주기
+//                Intent InternetIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://space.yonsei.ac.kr/index.php?lang=k"));
+//                startActivity(InternetIntent);
+//                finish();
+    }
+
+    private void getTeamlist() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("TEAM").child(uid);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                TeamnameList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    TeamnameList.add(dataSnapshot.getKey());
+                }
+                showTeams();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void showTeams() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("TEAM").child(uid);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                teamList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Team team = dataSnapshot.getValue(Team.class);
+                    for(String teamname : TeamnameList){
+                        if(team.getTeamName().equals(teamname)){
+                            Log.i("팀이름",teamname);
+                            teamList.add(team);
+                        }
+                    }
+                }
+                teamAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        uid = currentUser.getUid();
+        if(currentUser == null){
+//            로그인 안되어있음. 우선 로그인 페이지로 이동
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
